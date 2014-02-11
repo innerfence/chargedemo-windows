2b5acb1cfa7cb385684000d5f6fd2aa1e8e4c118
Toland Hon <toland@innerfence.com>
2014-02-11 12:25:03 -0800

    setup sample request

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -31,11 +31,59 @@ namespace InnerFence.ChargeDemo
 
         private void ChargeButton_Click(object sender, RoutedEventArgs e)
         {
-            // TODO: generate test request
-            ChargeRequest req = new ChargeRequest();
+            // Create the ChargeRequest using the default constructor
+            ChargeRequest chargeRequest = new ChargeRequest();
+
+            // 2-way Integration
+            //
+            // By supplying the ReturnURL parameter, we give Credit Card
+            // Terminal a way to invoke us when the transaction is
+            // complete. If you don't give a ReturnURL, Credit Card Terminal
+            // will still launch and pre-fill the form values supplied, but
+            // there will be no way for the user to return to your
+            // application.
+            //
+            // The simplest way to do this is just to set the property:
+            // chargeRequest.ReturnURL = @"com-innerfence-ChargeDemo://chargeResponse";
+            //
+            // But since it's so common to include app-specific parameters in
+            // the return URL, you can use the SetReturnURL()
+            // method to provide a dictionary of app-specific parameters which
+            // are automatically encoded into the query string of the
+            // ReturnURL. Those parameters will be available in the
+            // ExtraParams dictionary of the ChargeResponse when the request
+            // is completed.
+            //
+            // In this sample, we include an app-specific "record_id"
+            // parameter set to the value 123. You may call extra parameters
+            // anything you like, but to avoid collision with charge-related
+            // parameters, the names may not begin with "ifcc_".
+            Dictionary<string, string> extraParams =
+               new Dictionary<string, string>() { { "record_id", "123" } };
+            chargeRequest.SetReturnURL(
+                @"com-innerfence-ChargeDemo://chargeResponse",
+                extraParams
+            );
+
+            // Finally, we can supply customer and transaction data so that it
+            // will be pre-filled for submission with the charge.
+            chargeRequest.Address = "123 Test St";
+            chargeRequest.Amount = "50.00";
+            chargeRequest.Currency = "USD";
+            chargeRequest.City = "Nowhereville";
+            chargeRequest.Company = "Company Inc";
+            chargeRequest.Country = "US";
+            chargeRequest.Description = "Test transaction";
+            chargeRequest.Email = "johnexample.com";
+            chargeRequest.FirstName = "John";
+            chargeRequest.InvoiceNumber = "321";
+            chargeRequest.LastName = "Doe";
+            chargeRequest.Phone = "555-1212";
+            chargeRequest.State = "HI";
+            chargeRequest.Zip = "98021";
 
             // Submit request
-            req.Submit();
+            chargeRequest.Submit();
         }
 
         private async void HandleResponse()
