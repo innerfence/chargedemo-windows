f3ac32e4d96ca144318481083a9a4706c0eba934
Toland Hon <toland@innerfence.com>
2014-02-10 20:33:35 -0800

    implement ChargeRequest

--- ChargeAPI/ChargeRequest.cs
+++ ChargeAPI/ChargeRequest.cs
@@ -9,26 +9,129 @@ namespace InnerFence.ChargeAPI
 {
     public class ChargeRequest
     {
-        const string CCTERMINAL_BASE_URL = @"com-innerfence-ccterminal://charge/1.0.0/";
+        public const string CCTERMINAL_BASE_URL = @"com-innerfence-ccterminal://charge/1.0.0/";
+
+        public class ApplicationNotInstalled : Exception { }
+
+        public static class Keys
+        {
+            public const string ADDRESS = "address";
+            public const string AMOUNT = "amount";
+            public const string AMOUNT_FIXED = "amountFixed";
+            public const string CITY = "city";
+            public const string COMPANY = "company";
+            public const string COUNTRY = "country";
+            public const string CURRENCY = "currency";
+            public const string DESCRIPTION = "description";
+            public const string EMAIL = "email";
+            public const string FIRST_NAME = "firstName";
+            public const string INVOICE_NUMBER = "invoiceNumber";
+            public const string LAST_NAME = "lastName";
+            public const string PHONE = "phone";
+            public const string RETURN_APP_NAME = "returnAppName";
+            public const string RETURN_IMMEDIATELY = "returnImmediately";
+            public const string RETURN_URL = "returnURL";
+            public const string STATE = "state";
+            public const string TAX_RATE = "taxRate";
+            public const string ZIP = "zip";
+        }
 
         public ChargeRequest()
         {
+        }
 
+        public string Address { get; set; }
+        public string Amount { get; set; }
+        public string AmountFixed { get; set; }
+        public string City { get; set; }
+        public string Company { get; set; }
+        public string Country { get; set; }
+        public string Currency { get; set; }
+        public string Description { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string InvoiceNumber { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string ReturnAppName { get; set; }
+        public string ReturnImmediately { get; set; }
+        public string ReturnURL { get; set; }
+        public string State { get; set; }
+        public string TaxRate { get; set; }
+        public string Zip { get; set; }
+
+        public void SetReturnURL(string returnURL, Dictionary<string, string> extraParams)
+        {
+            returnURL += "?";
+
+            foreach (var param in extraParams)
+            {
+                if (param.Value != null)
+                {
+                    returnURL += String.Format(
+                        "{0}={1}&",
+                        Uri.EscapeUriString(param.Key),
+                        Uri.EscapeUriString(param.Value)
+                    );
+                }
+            }
+
+            this.ReturnURL = returnURL.TrimEnd('&');
+        }
+
+        public Dictionary<string, string> GenerateParams()
+        {
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+
+            parameters.Add(Keys.ADDRESS, this.Address);
+            parameters.Add(Keys.AMOUNT, this.Amount);
+            parameters.Add(Keys.AMOUNT_FIXED, this.AmountFixed);
+            parameters.Add(Keys.CITY, this.City);
+            parameters.Add(Keys.COMPANY, this.City);
+            parameters.Add(Keys.COUNTRY, this.Country);
+            parameters.Add(Keys.CURRENCY, this.Currency);
+            parameters.Add(Keys.DESCRIPTION, this.Description);
+            parameters.Add(Keys.EMAIL, this.Email);
+            parameters.Add(Keys.FIRST_NAME, this.FirstName);
+            parameters.Add(Keys.INVOICE_NUMBER, this.InvoiceNumber);
+            parameters.Add(Keys.LAST_NAME, this.LastName);
+            parameters.Add(Keys.PHONE, this.Phone);
+            parameters.Add(Keys.RETURN_APP_NAME, this.ReturnAppName);
+            parameters.Add(Keys.RETURN_IMMEDIATELY, this.ReturnImmediately);
+            parameters.Add(Keys.RETURN_URL, this.ReturnURL);
+            parameters.Add(Keys.STATE, this.State);
+            parameters.Add(Keys.TAX_RATE, this.TaxRate);
+            parameters.Add(Keys.ZIP, this.Zip);
+
+            return parameters;
         }
 
         public async void Submit()
         {
-            string uriToLaunch = @"com-innerfence-ccterminal://charge/1.0.0/";
+            string uriToLaunch = CCTERMINAL_BASE_URL + "?";
 
+            Dictionary<string, string> parameters = this.GenerateParams();
             // TODO: generate params
-            
+
+            foreach (var param in parameters)
+            {
+                if (param.Value != null)
+                {
+                    uriToLaunch += String.Format(
+                        "{0}={1}&",
+                        Uri.EscapeUriString(param.Key),
+                        Uri.EscapeUriString(param.Value)
+                    );
+                }
+            }
+
             // Launch the URI
-            var uri = new Uri(uriToLaunch);
+            var uri = new Uri(uriToLaunch.TrimEnd('&'));
             var success = await Launcher.LaunchUriAsync(uri);
 
-            if( !success )
+            if (!success)
             {
-                throw new InvalidOperationException("Failed to launch Credit Card Terminal. Please make sure app is installed.");
+                throw new ApplicationNotInstalled();
             }
         }
     }
