4b25d6880364e16102b0f9e3fc3e4bc9f1391325
Toland Hon <toland@innerfence.com>
2014-02-12 17:54:01 -0800

    windows app store and windows phone don't have any utility functions for dealing with query strings. implemented my own.

---
+++ ChargeAPI/Utils.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnerFence.ChargeAPI
+{
+    public static class Utils
+    {
+        public static Dictionary<string, string> ParseQueryString(string query)
+        {
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+
+            query = query.TrimStart('?');
+            if (!String.IsNullOrEmpty(query))
+            {
+                string[] paramArray = query.Split('&');
+                foreach (string param in paramArray)
+                {
+                    string[] paramPair = param.Split('=');
+                    string key = null;
+                    string value = null;
+
+                    if (paramPair.Length == 1)
+                    {
+                        key = paramPair[0];
+                    }
+                    else if (paramPair.Length == 2)
+                    {
+                        key = paramPair[0];
+                        value = paramPair[1];
+                    }
+                    else
+                    {
+                        throw new Exception(String.Format("Invalid param: {0}", param));
+                    }
+
+                    key = Uri.UnescapeDataString(key);
+                    value = Uri.UnescapeDataString(value);
+                    parameters[key] = value;
+                }
+            }
+            return parameters;
+        }
+
+        public static Uri UriWithAdditionalParams(Uri uri, Dictionary<string, string> newParameters)
+        {
+            Dictionary<string, string> parameters;
+            if (String.IsNullOrEmpty(uri.Query))
+            {
+                parameters = newParameters;
+            }
+            else
+            {
+                // extract existing query string parameters
+                parameters = Utils.ParseQueryString(uri.Query);
+
+                // add new parameters -- new parameters will overwrite old ones if key already exists
+                foreach (var param in newParameters)
+                {
+                    parameters[param.Key] = param.Value;
+                }
+            }
+
+            if (parameters.Count == 0)
+            {
+                // no parameters = no query string.
+                return uri;
+            }
+
+            // loop through parameters to generate param list
+            List<string> paramList = new List<string>();
+            foreach (var param in parameters)
+            {
+                if (param.Value != null)
+                {
+                    paramList.Add(
+                        String.Format("{0}={1}",
+                        Uri.EscapeDataString(param.Key),
+                        Uri.EscapeDataString(param.Value))
+                    );
+                }
+            }
+
+            // join param list with & and prefix it with ?
+            string queryString = String.Join("&", paramList);
+            queryString = String.Format("?{0}", queryString);
+
+            string uriString = String.Format(
+                "{0}://{1}{2}{3}",
+                uri.Scheme,
+                uri.Host,
+                uri.AbsolutePath,
+                queryString);
+
+            return new Uri(uriString);
+        }
+    }
+}
