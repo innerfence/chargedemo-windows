99e8b25074db6f1d67a1e145be59f332ec266cec
Toland Hon <toland@innerfence.com>
2014-02-24 14:33:34 -0800

    add ChargeAPI library

---
+++ InnerFence.ChargeDemo.Phone/ChargeAPI/ChargeUtilsPhone.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnerFence.ChargeAPI
+{
+    public static partial class ChargeUtils
+    {
+        public static void DeleteLocalData(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static object RetrieveLocalData(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void SaveLocalData(string key, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void SubmitChargeRequest(ChargeRequest chargeRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static uint GenerateRandomNumber()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
