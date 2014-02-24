ca27a582f4275d4f875ed0b324149f5ec91ab791
Toland Hon <toland@innerfence.com>
2014-02-24 12:11:36 -0800

    add empty windows phone project

---
+++ InnerFence.ChargeDemo.Phone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using ChargeDemo.Phone.Resources;
+
+namespace ChargeDemo.Phone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
